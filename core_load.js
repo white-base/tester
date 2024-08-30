b5f626387b5969191c706c0f79a03f473fe5bf49
logicfeel <logicfeel@gmail.com>
2024-08-30 17:14:37 +0900

    타입 로딩 확인

---
+++ core_load.js
@@ -0,0 +1,16 @@
+
+
+// var PropertyCollection = require('logic-core').PropertyCollection;
+
+import {PropertyCollection} from  'logic-core';
+
+
+// import {BindModel} from  'logic-bind-model';
+
+
+var p = new PropertyCollection();
+p.add()
+p.getObject()
+
+
+console.log('0');
