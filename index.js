e0344a6a3db3425a92ff316f8658e71109f95541
logic <logicfeel@gmail.com>
2025-08-25 17:50:13 +0900

    index.js: 'sto_master' 임포트 경로를 '@logicfeel/store-core'로 수정

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Kysely } from 'kysely';
 import { SQLTable } from 'logic-sql-entity';
-import { sto_master } from '@logic-entity/store-core';
+import { sto_master } from '@logicfeel/store-core';
 
 const major = "1.0.0";
 const prt_master = new SQLTable('PRT_Master');
