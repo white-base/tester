2397b5055e51eb074fbebf7bb07711decc87ca9e
logic <logicfeel@gmail.com>
2025-08-25 17:52:27 +0900

    prt_master에 'aa' 및 'bb' 컬럼 값 추가

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ import { sto_master } from '@logicfeel/store-core';
 const major = "1.0.0";
 const prt_master = new SQLTable('PRT_Master');
 
+prt_master.columns.addValue('aa', 10)
+prt_master.columns.addValue('bb', 10)
 
 export { sto_master, prt_master, major }
